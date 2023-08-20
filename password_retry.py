58866334a7957a3b1b13a645f35c25b737f12bb9
jeankuotw <19840920kuo@yahoo.com.tw>
2023-08-20 15:34:35 +0800

    update password_retry

--- password_retry.py
+++ password_retry.py
@@ -3,22 +3,42 @@
 # 輸入正確,印出'登入成功'
 # 輸入錯誤,印出'密碼錯誤,還有_次機會"
 
+#寫法1: num為輸入剩餘的機會
 password = 'a123456'
-num = 1 #輸入次數
+num = 3 #輸入機會
+
+while num >0:
+	keyin = input('請輸入密碼')
+
+	if keyin == password:
+		print('登入成功')
+		break
+	else:
+		num = num - 1 #輸入機會-1。此行也可以寫在while迴圈之下,不一定要放在else中
+		print('密碼錯誤!')
+		if num > 0: #還有輸入機會時,才會顯示此段
+			print('剩下',num,'次機會')
+		else: #沒有輸入機會時,顯示此段
+			print('無法登入,請與客服聯絡')
+
+
+#===寫法2: num為輸入的次數===
+#password = 'a123456'
+#num = 1 #輸入次數
 
 # 密碼輸入機會3次
-while num >= 1 and num < 4:
-	keyin = input('請輸入密碼(最多輸入3次):')
-	if keyin == password: #當使用者輸入內容input與密碼password符合
-		print('登入成功')
-		break #結束迴圈
-	else:
-		print('密碼錯誤,還剩下', 3-num, '次機會') #當密碼不符,提示還有幾次機會
-		num= num + 1 #輸入次數+1
-
-# 密碼錯誤輸入超過3次時
-while num >3 :
-	print('你已錯誤輸入',num-1,'次,無法登入') #程式到此行num=4,所以需-1,告知錯誤輸入的次數
-	break #結束迴圈,要不然num永遠>3會停不下來
-
-
+#while num <=3:
+#	keyin = input('請輸入密碼:')
+#
+#	if keyin == password: #當使用者輸入內容input與密碼password符合
+#		print('登入成功')
+#		break #結束迴圈
+#	else:
+#		print('密碼錯誤!!') #當密碼不符,提示還有幾次機會
+#
+#		if num <3:
+#			print('剩下',3-num, '次機會')
+#			num= num + 1 #輸入次數+1
+#		else:
+#			print('你已錯誤輸入',num,'次,請與客服聯繫') #程式到此行num=4,所以需-1,告知錯誤輸入的次數
+#			break #結束迴圈,要不然num永遠>3會停不下來
